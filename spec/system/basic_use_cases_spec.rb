6952596644e4009d3e5551bea9d91df8e7b3d918
Tamás Drahos <tamas.drahos@emarsys.com>
2020-05-29 21:52:21 +0200

    Add basic use case definitions

---
+++ spec/system/basic_use_cases_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+RSpec.describe "Basic use cases", :type => :system do
+  it "create a user account on one of the plans"
+  it "create a public or private app for a user"
+  it "set up custom limits for a public app"
+  it "opt out from the default public app limits (use the owner's plan instead)"
+  it "get the limits of an app"
+end
