0da8bb5d36f423bfa266ee3254069799d77256fa
Tamás Drahos <tamas.drahos@emarsys.com>
2020-05-30 02:33:03 +0200

    Finetune the basic use cases spec file

--- spec/system/basic_use_cases_spec.rb
+++ spec/system/basic_use_cases_spec.rb
@@ -3,22 +3,21 @@ require "spec_helper"
 RSpec.describe "Basic use cases:", :type => :system do
   let(:user) { User.new DEVELOPER_PLAN }
   let(:owners_plan_limits) { LimitsRepository.fetch DEVELOPER_PLAN }
-  let(:public_app_default_limits) { }
 
-  xit "create a user account on one of the plans" do
+  it "create a user account on one of the plans" do
     user = User.new DEVELOPER_PLAN
 
     expect(user.plan.name).to eq "Developer plan"
   end
 
-  xit "create a public or private app for a user" do
-    app = App.new user, APP::PUBLIC
+  it "create a public or private app for a user" do
+    app = App.new user, App::PUBLIC
 
     expect(app.privacy).to eq :public
   end
 
-  xcontext "for a public app" do
-    let(:app) { App.new owner, APP::PUBLIC }
+  context "for a public app" do
+    let(:app) { App.new user, App::PUBLIC }
     let(:custom_limits) { LimitConfig.new 6,75,2000, nil }
 
     it "set up custom limits" do
@@ -32,18 +31,29 @@ RSpec.describe "Basic use cases:", :type => :system do
     end
   end
 
-  xdescribe "get the limits of an app" do
+  describe "get the limits of an app" do
     context "when it is public" do
-      let(:app) { app = App.new owner, APP::PUBLIC }
-      it "shows the default public limits" do
+      let(:app) { App.new user, App::PUBLIC }
+      let(:public_app_default_limits) { LimitConfig.new 2, 45, nil, nil }
+
+      it "returns the default public limits" do
         expect(app.limits).to eq public_app_default_limits
+      end
+
+      context "and has custom config" do
+        before { LimitsRepository.store app, custom_limits }
+        let(:custom_limits) { LimitConfig.new 5, 75, 1000, nil }
+
+        it "returns the custom limits" do
+          expect(app.limits).to eq custom_limits
+        end
       end
     end
 
     context "when it is private" do
-      let(:app) { app = App.new owner, APP::PRIVATE }
+      let(:app) { App.new user, App::PRIVATE }
 
-      it "shows the limits of the app owner's plan" do
+      it "returns the limits of the app owner's plan" do
         expect(app.limits).to eq owners_plan_limits
       end
     end
